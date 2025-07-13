e6e72689e651aad6735f28c208f8aca6bad78805
Propertyl <linnikigor771@gmail.com>
2025-07-13 19:07:29 +0300

    restore Home files

---
+++ MiniGames/minigames/Home/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import {Link, Outlet} from "react-router-dom";
+
+export const App = () => {
+
+    return (
+        <div>
+            <Outlet/>
+        </div>
+    );
+};
